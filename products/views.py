02cf8ef637a2b93c20af9032f981905a2045d3bf
Goossage <igorgooss@gmail.com>
2021-06-24 14:23:20 +0300

    views index and products

--- products/views.py
+++ products/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def index(request):
+    return render(request, 'products/index.html')
+
+def products(request):
+    return render(request, 'products/products.html')
