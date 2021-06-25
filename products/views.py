aa0618532d2b589d3fb32dd11950a4d467e13f08
Goossage <igorgooss@gmail.com>
2021-06-25 18:26:27 +0300

    test-context

--- products/views.py
+++ products/views.py
@@ -7,3 +7,16 @@ def index(request):
 
 def products(request):
     return render(request, 'products/products.html')
+
+def test_context(request):
+    context = {
+        'title':'Store',
+        'header':'Welcome',
+        'username':'Anton Antano',
+        'products': [
+            {'name': 'Черный рюкзак Nike Heritage', 'price': '2340,00'},
+            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': '13590,00'},
+            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': '2890,00'},
+        ],
+    }
+    return render(request, 'products/test-context.html', context)
