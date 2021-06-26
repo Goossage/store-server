8c92fba9cdd66c7bae93ff3dfc281bf562a7bd39
Goossage <igorgooss@gmail.com>
2021-06-26 17:24:00 +0300

    inheritance templates

--- products/views.py
+++ products/views.py
@@ -3,20 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title':'Store'
+    }
+    return render(request, 'products/index.html', context)
 
 def products(request):
-    return render(request, 'products/products.html')
-
-def test_context(request):
     context = {
-        'title':'Store',
-        'header':'Welcome',
-        'username':'Anton Antano',
-        'products': [
-            {'name': 'Черный рюкзак Nike Heritage', 'price': '2340,00'},
-            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': '13590,00'},
-            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': '2890,00'},
-        ],
+        'title': 'Каталог'
     }
-    return render(request, 'products/test-context.html', context)
+    return render(request, 'products/products.html', context)
